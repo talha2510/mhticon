fabd2ffec4ed952340072d92e0b43505eca6a5d5
talha2510 <155014357+talha2510@users.noreply.github.com>
2024-10-10 07:56:45 -0700

    first commit

--- app/ui/home/home.tsx
+++ app/ui/home/home.tsx
@@ -24,29 +24,142 @@ export default function Home(){
 			</div>
 		</div>
 		</div>
+		
 		<div>
-			<h1>Our Clients</h1>
-			<hr/>
 			<div>
-				<Image src = "/kazema.png"
-				width = {100}
-				height = {0}
-				alt="kazema"
+				<p className={montserrat.className}>MHTICON is one of the most preferred staffing and HR solutions partners for many in the region. We have proven our services with our industry leading NPS (Net Promotor Score) of 74.7% in 2022. We hire and manage associates with world-class services to employers as well as their employees.</p>
+				<p className={montserrat.className}>Attracting and retaining employees is vital for all businesses. While you continue to scale your business, MHTICON will ensure you have the right talent to deliver that success.</p>
+				<p className={montserrat.className}>Our 40+ dedicated team of recruiters use their experience, tools, and resources, including in-depth market mapping and constantly updated market intelligence, to identify and recruit candidates for your business needs.</p>
+				<p className={montserrat.className}>With a wealth of proven successes behind us, we'll deliver the best people for the job - no matter the seniority.</p>
+			</div>
+			<div>
+				<Image
+				src="/home/home_image_1.jpg"
+				width={500}
+				height={1000}
+				alt="home intro image"
 				/>
-				<Image src = "/KCCEC.png"
-				width = {100}
-				height = {0}
-				alt="KCCEC"
-				/>
-				<Image src = "/atlantic_exports_ltd.png"
-				width = {100}
-				height = {0}
-				alt="atlantic_exports_ltd"/>
-				<Image src = "/rigid_industries.png"
-				width = {100}
-				height = {0}
-				alt="rigid_industries"
-				/>
+			</div>
+		</div>
+
+		<div>
+			<div>
+				<h1 className={lora.className}>Benefits of Outsourcing</h1>
+			</div>
+			<div>
+				<div>
+					<Image
+					src="/home/calculator.svg"
+					width={70}
+					height={70}
+					alt="calculator"
+					/>
+					<h1>Reduced cost</h1>
+				</div>
+				<div>
+					<Image
+					src="/home/group.svg"
+					width={70}
+					height={70}
+					alt="calculator"
+					/>
+					<h1>Fast hiring process</h1>
+				</div>
+				<div>
+					<Image
+					src="/home/degree.svg"
+					width={70}
+					height={70}
+					alt="calculator"
+					/>
+					<h1>Better talent quality</h1>
+				</div>
+				<div>
+					<Image
+					src="/home/Analytics.svg"
+					width={70}
+					height={70}
+					alt="calculator"
+					/>
+					<h1>Analytics & reporting</h1>
+				</div>
+				<div>
+					<Image
+					src="/home/quality.svg"
+					width={70}
+					height={70}
+					alt="calculator"
+					/>
+					<h1>100% compliance</h1>
+				</div>
+			</div>
+		</div>
+
+		<div>
+			<div>
+				<h1>OUR STRENGTH</h1>
+			</div>
+			<div>
+				<div>
+					<p>&nbsp;</p>
+					<h1>15+ years</h1>
+					<p>Experience</p>
+				</div>
+				<div>
+					<p>&nbsp;</p>
+					<h1>5500+ </h1>
+					<p>Outsourced Associates</p>
+				</div>
+				<div>
+					<p>Hired</p>
+					<h1>78+</h1>
+					<p>Nationalities</p>
+				</div>
+				<div>
+					<p>Operations in</p>
+					<h1>68+</h1>
+					<p>Countries</p>
+				</div>
+				<div>
+					<p>served</p>
+					<h1>500+</h1>
+					<p>Clients</p>
+				</div>
+			</div>
+		</div>
+
+		<div>
+			<div>
+				<h1>OUR Services</h1>
+			</div>
+			<div>\
+				<div>
+					<h1>CONTRACT STAFFING</h1>
+					<p>Short & long-term contract staffing solutions On our visa, payroll management & PRO outsourcing</p>
+				</div>
+				<div>
+					<h1>PERMANENT RECRUITMENT</h1>
+					<p>Hire local & global talent effectively</p>
+				</div>
+				<div>
+					<h1>HR OUTSOURCING & MANAGEMENT</h1>
+					<p>Leave it to experts all the HR policies, onboarding, & payroll</p>
+				</div>
+			</div>
+		</div>
+
+		<div>
+			<div>
+				<h1 className={montserrat.className}>WHY TASC?</h1>
+				<ul className={lora.className}>
+					<li>Get the right candidate resumes in 48 hours</li>
+					<li>An exclusive database of 900,000+ candidates</li>
+					<li>AI-enabled support technology</li>
+					<li>Offices in Dubai & Abu Dhabi</li>
+				</ul>
+			</div>
+			<div>
+				<h1 className={montserrat.className}>Work With The Top Recruitment Agency In UAE!</h1>
 			</div>
 		</div>
 	</div>);
